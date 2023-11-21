200b7d628485c67f835fed20a1ea2a8f75e12e87
jongyoung3 <jongyong5645@gmail.com>
2023-11-21 17:40:07 +0900

    first commit

--- src/test/java/com/mygroup/sbb/SbbApplicationTests.java
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -1,6 +1,8 @@
 package com.mygroup.sbb;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +17,7 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {        
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 }
